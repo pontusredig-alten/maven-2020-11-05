7b02091e459ed24dffdff5c8c85c9fdc8b656c1a
Pontus REDIG <pontus.redig@alten.se>
2020-11-09 10:16:05 +0100

    Upgraded to JUnit 5

--- src/main/java/se/iths/Calculator.java
+++ src/main/java/se/iths/Calculator.java
@@ -1,10 +1,13 @@
 package se.iths;
 
 public class Calculator {
+
     public int add(int a, int b) {
         return a + b;
     }
+
     public static void main(String[] args) {
+
         System.out.println("2 + 2 is " + new Calculator().add(2, 2) + ".");
     }
 }
--- src/test/java/se/iths/CalculatorTest.java
+++ src/test/java/se/iths/CalculatorTest.java
@@ -1,12 +1,17 @@
 package se.iths;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
+
     @Test
-    public void testAdd() {
+    void testAdd() {
         Calculator calculator = new Calculator();
         assertEquals(4, calculator.add(2, 2));
     }
+
+
 }
+
